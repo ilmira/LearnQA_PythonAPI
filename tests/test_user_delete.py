6d8fba3cfe44a71d32d140e9c7790e815e396e3c
Ilmira Idea <ilmiram625@gmail.com>
2025-09-19 17:35:24 +0300

    Added test_user_delete.py

---
+++ tests/test_user_delete.py
@@ -0,0 +1,103 @@
+import pytest
+
+from lib.my_requests import MyRequests
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+
+class TestUserDelete(BaseCase):
+    def test_delete_user_negative_first_users(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = MyRequests.post('/user/login', data=data)
+
+        auth_sid = self.get_cookie(response1, 'auth_sid')
+        token = self.get_header(response1, 'x-csrf-token')
+
+        response2 = MyRequests.delete('/user/2', headers={'x-csrf-token': token},
+                                      cookies={'auth_sid': auth_sid})
+
+        Assertions.assert_code_status(response2, 400)
+        assert response2.json()['error'] == 'Please, do not delete test users with ID 1, 2, 3, 4 or 5.'
+
+    def test_delete_user_just_created(self):
+        # REGISTER
+
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post('/user/', data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, 'id')
+
+        email = register_data['email']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, 'id')
+
+        # LOGIN
+
+        login_data = {
+            'email': email,
+            'password': password
+        }
+
+        response2 = MyRequests.post('/user/login', data=login_data)
+
+        auth_sid = self.get_cookie(response2, 'auth_sid')
+        token = self.get_header(response2, 'x-csrf-token')
+
+        # DELETE
+
+        response3 = MyRequests.delete(
+            f'/user/{user_id}',
+            headers={'x-csrf-token': token},
+            cookies={'auth_sid': auth_sid},
+        )
+
+        Assertions.assert_code_status(response3, 200)
+
+        # GET
+
+        response4 = MyRequests.get(
+            f'/user/{user_id}',
+            headers={'x-csrf-token': token},
+            cookies={'auth_sid': auth_sid},
+        )
+
+        Assertions.assert_code_status(response4, 404)
+
+
+    def test_delete_user_just_created_no_auth(self):
+        # REGISTER
+
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post('/user/', data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, 'id')
+
+        user_id = self.get_json_value(response1, 'id')
+
+        # LOGIN
+
+        login_data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response2 = MyRequests.post('/user/login', data=login_data)
+
+        auth_sid = self.get_cookie(response2, 'auth_sid')
+        token = self.get_header(response2, 'x-csrf-token')
+
+        # DELETE
+
+        response3 = MyRequests.delete(
+            f'/user/{user_id}',
+            headers={'x-csrf-token': token},
+            cookies={'auth_sid': auth_sid},
+        )
+
+        Assertions.assert_code_status(response3, 400)
